795ac4e7ef26f590d1ee192280e872f308d6f653
Geoov <george.geov7@gmail.com>
2022-04-22 12:33:54 -0700

    init project

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+app.use(cors());
+app.use(index);
+
+const io = socketIo(server, { cors: { origin: "*" }});
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
